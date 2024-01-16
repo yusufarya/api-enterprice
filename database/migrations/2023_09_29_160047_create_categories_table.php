10a238e399a4f62ac98cf80ab8d6155a85a505bb
yusufarya <aryaherby29nov2k@gmail.com>
2024-01-16 23:46:22 +0700

    change table name

--- database/migrations/2023_09_29_160047_create_categories_table.php
+++ database/migrations/2023_09_29_160047_create_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id_category');
             $table->string('name', 60);
             $table->string('created_by', 50);
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('categories');
     }
 };
